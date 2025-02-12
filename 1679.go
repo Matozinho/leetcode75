61078b4a70654a7eb81e371a8a1cc08918f3cb2d
Felipi Lima Matozinho <50120388+Matozinho@users.noreply.github.com>
2025-02-12 02:11:18 +0000

    startup

---
+++ 1679.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func MaxOperations(nums []int, k int) int {
+	for _, num := range nums {
+		fmt.Println(num)
+	}
+
+	return 0
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matozinho/leetcode75
+
+go 1.23.4
